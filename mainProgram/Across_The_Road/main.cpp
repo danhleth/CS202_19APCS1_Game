4f5cc53db354428505b3ed2367b983edd62afcc9
maiquockhanh <maiquockhanh2609@gmail.com>
2020-11-28 01:03:34 +0700

    delete file

--- mainProgram/Across_The_Road/main.cpp
+++ mainProgram/Across_The_Road/main.cpp
@@ -1,15 +1,33 @@
 #include "includePath.h"
-#include "thread.cpp"
+#include <SFML/Graphics.hpp>
+#include <limits>
 
 int main() {
-    thread t1(ThreadFunc1);
-    while (1) {
-        system("PAUSE");
-        /*int temp = toupper(cin.get());
-        if (temp == 27) {
-            exitGame(&t1); 
-            return 0;
-        }*/
+    sf::RenderWindow window(sf::VideoMode(900, 500), "SFML-Tutorial", sf::Style::Close);
+    window.setFramerateLimit(60);
+
+    sf::RectangleShape rectang(sf::Vector2f(400, 220));
+
+    rectang.setPosition(250, 140);
+    rectang.setFillColor(sf::Color::Yellow);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::EventType::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.draw(rectang);
+
+        window.display();
     }
+
     return 0;
 }
