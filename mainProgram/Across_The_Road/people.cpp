143aa393c6b2c264d046ce95f928f0e32eaa4872
DanhLe1412 <Dragonfly0165@gmail.com>
2020-11-29 19:16:34 +0700

    them people.h va people.cpp

---
+++ mainProgram/Across_The_Road/people.cpp
@@ -0,0 +1,32 @@
+#include "people.h"
+
+void PEOPLE::Up(int)
+{
+}
+
+void PEOPLE::Left(int)
+{
+}
+
+void PEOPLE::Right(int)
+{
+}
+
+void PEOPLE::Down(int)
+{
+}
+
+bool PEOPLE::isImpact(const Vehicle*&)
+{
+    return false;
+}
+
+bool PEOPLE::isFinish()
+{
+    return false;
+}
+
+bool PEOPLE::isDead()
+{
+    return false;
+}
