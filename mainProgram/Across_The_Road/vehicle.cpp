d802197512c414e90d2818aef65a52f0a88becba
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2020-11-29 18:31:18 +0700

    draw Car / fit Truck - demo

--- mainProgram/Across_The_Road/vehicle.cpp
+++ mainProgram/Across_The_Road/vehicle.cpp
@@ -5,14 +5,13 @@ Vehicle::Vehicle() {
 	mX = 0;
 	mY = 0;
 }
-
 Vehicle::Vehicle(float x, float y) {
 	mX = x;
 	mY = y;
 }
 
 void Vehicle::Move(float _x, float _y) {
-
+	return;
 }
 
 float Vehicle::X() {
@@ -29,10 +28,10 @@ void Vehicle::Y(float y) {
 }
 
 	//Truck
-Truck::Truck() {}
+Truck::Truck() 
+	:Vehicle() {}
 Truck::Truck(float x, float y) 
 	:Vehicle(x,y) {}
-
 void Truck::Move(float _x, float _y) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
 		convex.move(_x, 0);
@@ -61,6 +60,31 @@ void Truck::Draw(sf::RenderWindow &window) {
 }
 
 	//Car
+Car::Car() 
+	: Vehicle() {}
+Car::Car(float _x, float _y)
+	: Vehicle(_x, _y) {}
 void Car::Move(float _x, float _y) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+		convex.move(_x, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+		convex.move(-_x, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+		convex.move(0, _y);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+		convex.move(0, -_y);
+	}
+}
+void Car::Disappear(sf::RenderWindow&) {
 
+}
+void Car::Draw(sf::RenderWindow& window) {
+	convex.setPointCount(shape.size());
+	for (int i = 0; i < shape.size(); i++) {
+		convex.setPoint(i, sf::Vector2f(X() + shape[i][0], Y() + shape[i][1]));
+	}
+	window.draw(convex);
 }
--- mainProgram/Across_The_Road/vehicle.h
+++ mainProgram/Across_The_Road/vehicle.h
@@ -22,7 +22,7 @@ class Truck : public Vehicle {
 private:
 	sf::ConvexShape convex;
 	vector<vector<float>> shape = {
-		{50.f, 50.f},{0.f, 50.f},{0.f, 100.f},{100.f, 100.f},{100.f, 0.f},{50.f, 0.f}
+		{40.f, 40.f},{0.f, 40.f},{0.f, 80.f},{150.f, 80.f},{150.f, 0.f},{40.f, 0.f}
 	};
 public:
 	Truck();
@@ -33,9 +33,17 @@ public:
 };
 
 class Car : public Vehicle {
-
+private:
+	sf::ConvexShape convex;
+	vector<vector<float>> shape = {
+		{15.f, 30.f},{0.f, 30.f},{0.f, 55.f},{100.f, 55.f},{100.f, 30.f},{85.f, 30.f}, {70.f, 0.f}, {25.f, 0.f}
+	};
 public:
+	Car();
+	Car(float, float);
 	void Move(float, float);
+	void Disappear(sf::RenderWindow&);
+	void Draw(sf::RenderWindow&);
 };
 
 #endif
